b360336db97d36fd3bafa4bc38f372015bf90178
Aditya Sundaresan <sundaresanaditya@gmail.com>
2021-10-20 09:41:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 function myFunction(dataToSend) {
-    var url = "'https://cors.io/?https://things.ubidots.com/api/v1.6/devices/RasPi/";
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("X-Auth-Token", "BBFF-2BoQtMzRrAltynSTpJK7JtbsB3oQ46");
-    xhr.setRequestHeader("Authorization", "Bearer BBFF-2BoQtMzRrAltynSTpJK7JtbsB3oQ46");
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    //xhr.setRequestHeader("Access-Control-Allow-Origin", "https://adisundi.github.io");
-    //xhr.setRequestHeader("Origin", "https://adisundi.github.io");
-
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            console.log(xhr.status);
-            console.log(xhr.responseText);
-        }
-    };
-
-    var data = `{"drinkno":4,
-     
-    }`;
-
-    xhr.send(data);
-
-    /*
     var url = "https://cors-anywhere.herokuapp.com/https://things.ubidots.com/api/v1.6/devices/RasPi/";
 
     var xhr = new XMLHttpRequest();
@@ -41,11 +15,13 @@ function myFunction(dataToSend) {
             console.log(xhr.responseText);
         }
     };
+    let timest = Date.now()
 
-    var data = `{"drinkno":${dataToSend},
-                 "timestamp":${Date.now()},
-                }`;
-    console.log(data);
-    xhr.send(data);
-    */
+    var data = {
+        "drinkno": dataToSend,
+        "timestamp": timest
+    };
+    console.log(JSON.stringify(data));
+    xhr.send(JSON.stringify(data));
+
 }
