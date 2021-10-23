e2fea7730457d75557572b22744aeabf109c2a82
Aditya Sundaresan <sundaresanaditya@gmail.com>
2021-10-23 14:35:58 +0800

    Final functionalities and styles added

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function myFunction(dataToSend) {
+let intToSend = 0;
+
+function myFunction(drinkCode) {
+    if (intToSend > 10) {
+        intToSend = 0;
+    }
+    if (0 < intToSend && intToSend < 10) {
+        intToSend = intToSend + (10 * drinkCode);
+        alert("Your order has been placed!");
+        alert(`sending${intToSend}`);
+        sendData(intToSend);
+    }
+    if (intToSend == 0) {
+        intToSend = drinkCode;
+        alert("Choose your second drink!");
+    }
+}
+
+function sendData(dataToSend) {
 
     var url = "https://cors-anywhere.herokuapp.com/https://things.ubidots.com/api/v1.6/devices/RasPi/";
 
